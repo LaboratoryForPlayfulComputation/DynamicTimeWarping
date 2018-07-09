f43962b1b7d4bd947c655fd0518dc49cc418f248
Bridget Murphy <stormfur9@gmail.com>
2018-07-09 14:00:51 -0600

    Added more test cases (third, fourth, and fifth)

--- DynamicTimeWarpingTests/DynamicTimeWarpingTests.swift
+++ DynamicTimeWarpingTests/DynamicTimeWarpingTests.swift
@@ -14,13 +14,19 @@ class DynamicTimeWarpingTests: XCTestCase {
     var dtw: DTW!
     var first: [(Double,Double,Double)]!
     var second: [(Double,Double,Double)]!
-    
+    var third: [(Double,Double,Double)]!
+    var fourth: [(Double,Double,Double)]!
+    var fifth: [(Double,Double,Double)]!
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         dtw = DTW()
         first = [(1.0,1.0,1.0),(2.0,2.0,2.0),(3.0,3.0,3.0),(4.0,4.0,4.0),(5.0,5.0,5.0)]
         second = [(1.0,1.0,1.0),(3.0,3.0,3.0),(4.0,4.0,4.0),(5.0,5.0,5.0)]
+        third = [(0.0,0.0,0.0),(0.0,0.0,0.0),(0.0,0.0,0.0),(0.0,0.0,0.0),(0.0,0.0,0.0)]
+        fourth = [(1.0,1.0,1.0),(1.0,2.0,2.0),(3.0,3.0,3.0),(4.0,4.0,4.0),(5.0,5.0,5.0)]
+        fifth = [(1.0,1.0,1.0),(2.0,2.0,2.0),(3.0,3.0,3.0),(4.0,4.0,4.0),(5.0,5.0,5.0)]
     }
     
     override func tearDown() {
@@ -28,11 +34,36 @@ class DynamicTimeWarpingTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testDifferent() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         let distance = dtw.dtw(totAcc: first, newAcc: second)
-        XCTAssertNotEqual(distance,0.0, "This is just a guess")
+        //let distance2 = dtw.dtw()
+        XCTAssertNotEqual(distance,0.0, "Testing the first array against one with no array of 2's")
+    }
+    
+    func testSame() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let distance = dtw.dtw(totAcc: first, newAcc: fifth)
+        //let distance2 = dtw.dtw()
+        XCTAssertEqual(distance,0.0, "Two of the same arrays being tested")
+    }
+    
+    func testAllZeros() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let distance = dtw.dtw(totAcc: first, newAcc: third)
+        //let distance2 = dtw.dtw()
+        XCTAssertEqual(distance,sqrt(588), "testing case 1 against an array of all zeros")
+    }
+    
+    func testMixOnesandTwos() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let distance = dtw.dtw(totAcc: first, newAcc: fourth)
+        //let distance2 = dtw.dtw()
+        XCTAssertEqual(distance,1.0, "Testing array 1 against an array different by 1 ")
     }
     
 }
