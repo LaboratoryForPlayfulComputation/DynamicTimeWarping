ba24a3884f2f6b3f2d8b57fa4f75173ae2b3b6d2
Bridget Murphy <Bridget@Bridgets-MBP.research>
2018-07-09 11:50:39 -0600

    initial commit

--- DynamicTimeWarpingTests/DynamicTimeWarpingTests.swift
+++ DynamicTimeWarpingTests/DynamicTimeWarpingTests.swift
@@ -11,9 +11,16 @@ import XCTest
 
 class DynamicTimeWarpingTests: XCTestCase {
     
+    var dtw: DTW!
+    var first: [(Double,Double,Double)]!
+    var second: [(Double,Double,Double)]!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        dtw = DTW()
+        first = [(1.0,1.0,1.0),(2.0,2.0,2.0),(3.0,3.0,3.0),(4.0,4.0,4.0),(5.0,5.0,5.0)]
+        second = [(1.0,1.0,1.0),(3.0,3.0,3.0),(4.0,4.0,4.0),(5.0,5.0,5.0)]
     }
     
     override func tearDown() {
@@ -24,13 +31,8 @@ class DynamicTimeWarpingTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let distance = dtw.dtw(totAcc: first, newAcc: second)
+        XCTAssertNotEqual(distance,0.0, "This is just a guess")
     }
     
 }
